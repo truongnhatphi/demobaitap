745f043b19c83300af7222a8e44d6890c2843938
truongnhatphi <55117983+truongnhatphi@users.noreply.github.com>
2020-04-23 12:17:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<script>
+    function getdata()
+	{
+		var a;
+		a = <?php echo $1?>;
+		document.getElementById("ketqua").value = a;
+	}
+</script>
+
 <!DOCTYPE html>
 <html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -5,9 +14,44 @@
   <meta charset="utf-8">
 </head>
 <body>
-
-  <h1>Cloud-Php-Ex000</h1>
-  <p>Chạy thử 1.</p>
+  <?php
+	if(isset($_GET["a"]) && isset($_GET["b"]) && isset($_GET["c"]))
+		{
+			$a = $_GET["a"];
+			$b = $_GET["b"];
+			$c = $_GET["c"];
+			if( $a +$b > $c && $a+$c>$b && $b+$c > $a)
+			{
+				if($a==$b && $b==$c)
+				$1 = "Tam Giác Đều ";
+				elseif($a==$b || $a==$c || $b==$c )
+				$1 = "Tam Giác Cân ";
+				elseif(pow($a,2)==pow($b,2)+pow($c,2)||pow($b,2)==pow($c,2)+pow($c,2)|| pow($c,2)==pow($b,2)+pow($a,2))
+				$1 = "Tam Giác Vuông ";
+				elseif(($a==$b || $a==$c || $b==$c ) && (pow($a,2)==pow($b,2)+pow($c,2)||pow($b,2)==pow($c,2)+pow($c,2)|| pow($c,2)==pow($b,2)+pow($a,2)))
+				$1 = "Tam Giác Vuông Cân ";
+				else
+				$1 = "Tam Giác Thường ";
+			}
+			else
+			$1 = " Đây không phải tam giác ";
+		}
+		
+?>
+  <h1>Xét Tam giác :</h1>
+ 
+  <form method="GET">
+<div> Nhập canh A </div>
+<input name="a" type="text" id ="a" value="<?php echo $_GET["a"];?>" />
+<br>
+<div> Nhập canh B </div>
+<input name="b" type="text" id ="b" value="<?php echo $_GET["b"];?>" />
+<br>
+<div> Nhập canh C </div>
+<input name="c" type="text" id ="c" value="<?php echo $_GET["c"];?>" />
+<input name="ketqua">
+<input type="button" name="check" onClick="getdata()" />
+</form>
 
 </body>
 </html>
